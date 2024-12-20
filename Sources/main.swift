d6d27aa0d7d93c970e4029ad97e8ae0d961d2259
= <support@mrsoir.com>
2024-12-20 10:45:42 +0100

    parsing on each benchmark iteration

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,15 +1,17 @@
 import Foundation
 
+let puzzle_1 = data
 let day01 = Day01()
 
 testPerformance()
 
 func testPerformance() {
     var runtimeAvg = Double(0)
-    let runs = 1000
+    let runs = 1_000
     for run in 0..<runs {
         let start = Date()
-        let (d10, d11) = day01.run()
+        // make sure the puzzle is parsed on each iteration:
+        let (d10, d11) = day01.run(puzzle: puzzle_1)
         let elapsed = Date().timeIntervalSince(start)
         runtimeAvg += Double(elapsed)
         print(String(format: "run[%d / %d]: Day 1 - result 1: %d, result 2: %d - elapsed: %.3f ms", run, runs, d10, d11, elapsed * 1000))
---
+++ release.sh
@@ -0,0 +1,1 @@
+.build/release/Day1
